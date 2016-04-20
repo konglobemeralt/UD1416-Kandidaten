09b025fed823f7c2238f7c5a9b5dc319b9e5291c
NordicGamer <stahlberg.erik@yahoo.se>
2016-04-20 15:27:25 +0200

    ADDED Text now render to a srv and are used to render to a quad.

--- Project/Classes/Text.cpp
+++ Project/Classes/Text.cpp
@@ -29,7 +29,13 @@ void Text::Render() {
 
 	gdeviceContext->IASetVertexBuffers(0, 1, m_graphicsManager->getQuad(), &vertexSize, &offset);
 
+	RenderText();
+
+	gdeviceContext->PSSetShaderResources(0, 1, &m_graphicsManager->thesisData.shaderResourceViews["Text"]);
+
 	gdeviceContext->Draw(4, 0);
+
+	gdeviceContext->PSSetShaderResources(0, 1, &m_graphicsManager->thesisData.shaderResourceViews["NULL"]);
 }
 
 void Text::Initialize() {
@@ -115,7 +121,7 @@ void Text::Initialize() {
 	m_graphicsManager->createTexture2D("myRTVandSRV");
 
 	// Add image on an SRV (base filepath will be set to the assets folder automatically)
-	m_graphicsManager->attachImage("ToneMapping/Images/picture.jpg", "mySRV");
+	//m_graphicsManager->attachImage("ToneMapping/Arches_E_PineTree_Preview.jpg", "mySRV");
 
 
 
@@ -129,4 +135,144 @@ void Text::Initialize() {
 	//	);
 
 	m_graphicsManager->createSamplerState("CoolSampler", D3D11_FILTER_MIN_MAG_MIP_POINT, D3D11_TEXTURE_ADDRESS_WRAP);
+
+	// Initialize Systems
+	InitializeDirect2D();
+	InitializeDirectWrite();
+}
+
+void Text::InitializeDirect2D()
+{
+	// Factory
+	CheckStatus(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_d2dFactory), L"D2D1CreateFactory");
+
+	// Device & DeviceContext
+	CheckStatus(gdevice->QueryInterface(__uuidof(IDXGIDevice), (void **)&m_dxgiDevice), L"QueryInterface");
+	CheckStatus(m_d2dFactory->CreateDevice(m_dxgiDevice, &m_d2dDev), L"CreateDevice");
+	CheckStatus(m_d2dDev->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &m_d2dDevcon), L"CreateDeviceContext");
+
+	// Rendertarget
+	D3D11_TEXTURE2D_DESC texDesc;
+	texDesc.ArraySize = 1;
+	texDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	texDesc.Height = m_graphicsManager->getWindowHeight();
+	texDesc.Width = m_graphicsManager->getWindowWidth();
+	texDesc.MipLevels = 1;
+	texDesc.MiscFlags = 0;
+	texDesc.SampleDesc.Count = 1;
+	texDesc.SampleDesc.Quality = 0;
+	texDesc.Usage = D3D11_USAGE_DEFAULT;
+	ID3D11Texture2D* d2dTextureTarget;
+	CheckStatus(gdevice->CreateTexture2D(&texDesc, NULL, &d2dTextureTarget), L"CreateTexture2D");
+
+	d2dTextureTarget->QueryInterface(&m_idxgSurface);
+	D2D1_RENDER_TARGET_PROPERTIES props =
+		D2D1::RenderTargetProperties(
+			D2D1_RENDER_TARGET_TYPE_DEFAULT,
+			D2D1::PixelFormat(DXGI_FORMAT_UNKNOWN, D2D1_ALPHA_MODE_PREMULTIPLIED),
+			96,
+			96);
+	CheckStatus(m_d2dFactory->CreateDxgiSurfaceRenderTarget(m_idxgSurface, &props, &m_d2dRenderTarget), L"CreateDxgiSurfaceRenderTarget");
+	m_graphicsManager->thesisData.shaderResourceViews["NULL"] = nullptr;
+
+	// Brushes
+	CheckStatus(m_d2dRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Black), &m_blackBrush), L"CreateSolidColorBrush");
+	CheckStatus(m_d2dRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Orange), &m_orangeBrush), L"CreateSolidColorBrush");
+
+	// Create the render target view.
+	D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
+	renderTargetViewDesc.Format = texDesc.Format;
+	renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	renderTargetViewDesc.Texture2D.MipSlice = 0;
+	ID3D11RenderTargetView* rtv;
+	CheckStatus(gdevice->CreateRenderTargetView(d2dTextureTarget, &renderTargetViewDesc, &rtv), L"CreateRenderTargetView");
+	m_graphicsManager->thesisData.renderTargetViews["Text"] = rtv;
+
+	// Setup the description of the shader resource view.
+	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
+	shaderResourceViewDesc.Format = texDesc.Format;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+	shaderResourceViewDesc.Texture2D.MipLevels = 1;
+	ID3D11ShaderResourceView* srv;
+	CheckStatus(gdevice->CreateShaderResourceView(d2dTextureTarget, &shaderResourceViewDesc, &srv), L"CreateShaderResourceView");
+	m_graphicsManager->thesisData.shaderResourceViews["Text"] = srv;
+}
+
+void Text::InitializeDirectWrite()
+{
+	// Factory
+	CheckStatus(DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED,
+		__uuidof(IDWriteFactory),
+		reinterpret_cast<IUnknown**>(&m_writeFactory)),
+		L"DWriteCreateFactory");
+
+	// Font
+	m_text = L"KHK";
+	m_textLength = (UINT32)wcslen(m_text);
+
+	CheckStatus(m_writeFactory->CreateTextFormat(
+		L"Arial",
+		NULL,
+		DWRITE_FONT_WEIGHT_REGULAR,
+		DWRITE_FONT_STYLE_NORMAL,
+		DWRITE_FONT_STRETCH_NORMAL,
+		500.0f,
+		L"en-us",
+		&m_writeTextFormat),
+		L"CreateTextFormat");
+
+	// Center align the text.
+	CheckStatus(m_writeTextFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER), L"SetTextAlignment");
+	CheckStatus(m_writeTextFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER), L"SetParagraphAlignment");
+
+	// layout to draw on
+	m_layoutRect = D2D1::RectF(
+		static_cast<FLOAT>(0.0f),
+		static_cast<FLOAT>(0.0f),
+		static_cast<FLOAT>(m_graphicsManager->getWindowWidth()),
+		static_cast<FLOAT>(m_graphicsManager->getWindowHeight())
+	);
+}
+
+void Text::RenderText()
+{
+	m_d2dRenderTarget->BeginDraw();
+	m_d2dRenderTarget->SetTransform(D2D1::IdentityMatrix());
+	m_d2dRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+	// Call the DrawText method of this class.
+	m_d2dRenderTarget->DrawText(
+		m_text,				// The string to render.
+		m_textLength,			// The string's length.
+		m_writeTextFormat,    // The text format.
+		m_layoutRect,			// The region of the window where the text will be rendered.
+		m_orangeBrush			// The brush used to draw the text.
+	);
+	m_d2dRenderTarget->EndDraw();
+}
+
+void Text::CheckStatus(HRESULT hr, LPCTSTR titel)
+{
+	if (hr == E_ABORT)
+		MessageBox(NULL, L"HREULT = E_ABORT", titel, NULL);
+	else if(hr == E_ACCESSDENIED)
+		MessageBox(NULL, L"HREULT = E_ACCESSDENIED", titel, NULL);
+	else if (hr == E_FAIL)
+		MessageBox(NULL, L"HREULT = E_FAIL", titel, NULL);
+	else if (hr == E_HANDLE)
+		MessageBox(NULL, L"HREULT = E_HANDLE", titel, NULL);
+	else if (hr == E_INVALIDARG)
+		MessageBox(NULL, L"HREULT = E_INVALIDARG", titel, NULL);
+	else if (hr == E_NOINTERFACE)
+		MessageBox(NULL, L"HREULT = E_NOINTERFACE", titel, NULL);
+	else if (hr == E_NOTIMPL)
+		MessageBox(NULL, L"HREULT = E_NOTIMPL", titel, NULL);
+	else if (hr == E_OUTOFMEMORY)
+		MessageBox(NULL, L"HREULT = E_OUTOFMEMORY", titel, NULL);
+	else if (hr == E_POINTER)
+		MessageBox(NULL, L"HREULT = E_POINTER", titel, NULL);
+	else if (hr == E_UNEXPECTED)
+		MessageBox(NULL, L"HREULT = E_UNEXPECTED", titel, NULL);
 }
--- Project/Classes/Text.h
+++ Project/Classes/Text.h
@@ -1,6 +1,13 @@
 #ifndef H_TEXT
 #define H_TEXT
 
+#include <d2d1_1.h>
+#include <d2d1_1helper.h>
+#include <dwrite.h>
+#include <atlstr.h>
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "Dwrite")
+
 //#include "GraphicsManager.h"
 
 //#define manager GraphicsManager::getInstance()
@@ -19,6 +26,29 @@ public:
 
 private:
 	GraphicsManager* m_graphicsManager;
+
+	void InitializeDirect2D();
+	void InitializeDirectWrite();
+	void CheckStatus(HRESULT hr, LPCTSTR titel);
+	void RenderText();
+
+	// Direct2D
+	ID2D1Factory1* m_d2dFactory;
+	IDXGIDevice* m_dxgiDevice;
+	ID2D1Device* m_d2dDev;
+	ID2D1DeviceContext* m_d2dDevcon;
+	IDXGISurface* m_idxgSurface;
+	ID2D1RenderTarget* m_d2dRenderTarget;
+	ID2D1SolidColorBrush* m_blackBrush;
+	ID2D1SolidColorBrush* m_orangeBrush;
+
+	// DirectWrite
+	IDWriteFactory* m_writeFactory;
+	const wchar_t* m_text;
+	UINT32 m_textLength;
+	D2D1_RECT_F m_layoutRect;
+	IDWriteTextFormat* m_writeTextFormat;
+	RECT rc;
 };
 
 #endif
