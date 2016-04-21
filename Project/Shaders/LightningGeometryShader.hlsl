d4324b252c7e988dba4bd7f32fa84cc1f6bb5221
T-quix <tobbejo@hotmail.com>
2016-04-21 09:35:04 +0200

    Window Manager added and camera  movement lightning

--- Project/Shaders/LightningGeometryShader.hlsl
+++ Project/Shaders/LightningGeometryShader.hlsl
@@ -5,7 +5,7 @@ struct DS_OUT
 };
 
 [maxvertexcount(8)]	//defines the output to be a maximum of x vertices.
-void GS_main(line DS_OUT input[2], inout TriangleStream<DS_OUT> OutputStream)
+void GS_main(triangle DS_OUT input[3], inout TriangleStream<DS_OUT> OutputStream)
 {
 	float4 lineDirVec; //Line Direction Vector
 	float4 perpendicularVec;
@@ -17,33 +17,34 @@ void GS_main(line DS_OUT input[2], inout TriangleStream<DS_OUT> OutputStream)
 
 	//perpendicularVec = XMVector3Orthogonal(lineDirVec);	//Not working in HLSL
 
-	float4 v[4];
-	v[0] = float4(input[1].Pos + float3(0.1f, -0.1f, 0.0f), 1.0f);
-	v[1] = float4(input[1].Pos + float3(0.1f, 0.1f, 0.0f), 1.0f);
-	v[2] = float4(input[0].Pos + float3(-0.1f, -0.1f, 0.0f), 1.0f);
-	v[3] = float4(input[0].Pos + float3(-0.1f, 0.1f, 0.0f), 1.0f);
 
-	for (uint i = 0; i < 4; i++)
-	{
-		DS_OUT output; //Output of type "struct DS_OUT"
+	//float4 v[4];
+	//v[0] = float4(input[1].Pos + float3(0.1f, -0.1f, 0.0f), 1.0f);
+	//v[1] = float4(input[1].Pos + float3(0.1f, 0.1f, 0.0f), 1.0f);
+	//v[2] = float4(input[0].Pos + float3(-0.1f, -0.1f, 0.0f), 1.0f);
+	//v[3] = float4(input[0].Pos + float3(-0.1f, 0.1f, 0.0f), 1.0f);
 
-		output.Pos = v[i];
-		output.Tex = input[0].Tex;
-
-		OutputStream.Append(output);
-	}
-	OutputStream.RestartStrip();
-
-
-	//for (uint i = 0; i < 2; i++)
+	//for (uint i = 0; i < 4; i++)
 	//{
 	//	DS_OUT output; //Output of type "struct DS_OUT"
 
-	//	output.Pos = input[i].Pos;
-	//	output.Tex = input[i].Tex;
+	//	output.Pos = v[i];
+	//	output.Tex = input[0].Tex;
 
 	//	OutputStream.Append(output);
 	//}
+	//OutputStream.RestartStrip();
 
-	//OutputStream.RestartStrip(); //If i want a brand new Triangle
+
+	for (uint i = 0; i < 3; i++)
+	{
+		DS_OUT output; //Output of type "struct DS_OUT"
+
+		output.Pos = input[i].Pos;
+		output.Tex = input[i].Tex;
+
+		OutputStream.Append(output);
+	}
+
+	OutputStream.RestartStrip(); //If i want a brand new Triangle
 }
