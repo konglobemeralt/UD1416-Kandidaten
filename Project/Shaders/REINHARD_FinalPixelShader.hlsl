b4f33591fa609907b6a08d62e705cfd6e863a2c6
Huffo <lukas.schachtschabel@live.se>
2016-04-25 15:48:47 +0200

    Reinhard work in progress

--- Project/Shaders/REINHARD_FinalPixelShader.hlsl
+++ Project/Shaders/REINHARD_FinalPixelShader.hlsl
@@ -1,4 +1,6 @@
-Texture2D FirstSRV;
+Texture2D Texture : register(t0);
+Texture2D AvgLumSRV : register(t1);
+Texture2D MaxLumSRV : register(t2);
 sampler SamplerWrap;
 
 cbuffer mipLevel {
@@ -10,45 +12,54 @@ struct VS_OUT {
 	float2 Tex : TEXCOORD;
 };
 
+float ldexp(float x, float exp) {
+	return x * pow(2.0, exp);
+}
+
+float3 RGBEToRGB(float4 rgbe) {
+	if (rgbe.w > 0.0) {
+		rgbe *= 255.0;
+		float val = ldexp(1.0, rgbe.w - (128.0 + 8.0));
+		return rgbe.xyz * val;
+	}
+	return float3(0.0, 0.0, 0.0);
+}
+
+float GetLuminance(float3 rgb) {
+	return (0.2126 * rgb.r) + (0.7152 * rgb.g) + (0.0722 * rgb.b);
+}
+
+float3 RGB2xyY(float3 rgb) {
+	const float3x3 RGB2XYZ = float3x3(	0.4124, 0.3576, 0.1805, 
+										0.2126, 0.7152, 0.0722, 
+										0.0193, 0.1192, 0.9505);
+	float3 XYZ = mul(RGB2XYZ, rgb);
+	return float3(XYZ.x / (XYZ.x + XYZ.y + XYZ.z), XYZ.y / (XYZ.x + XYZ.y + XYZ.z), XYZ.y);
+}
+
+float3 xyY2RGB(float3 xyY) {
+	float3 XYZ = float3(	xyY.x * (xyY.z / xyY.y),
+							xyY.z, 
+							(1.0 - xyY.x - xyY.y) * (xyY.z / xyY.y)	);
+	const float3x3 XYZ2RGB = float3x3(	3.2406,		-1.5372,	-0.4986,
+										-0.9689,	1.8758,		0.0415, 
+										0.0557,		-0.2040,	1.0570);
+	return mul(XYZ2RGB, XYZ);
+}
+
 float4 PS_main(VS_OUT input) : SV_TARGET
 {
-	float key = 0.25; //exposure setting
-	float d = 1.0;
-	float3 tex = FirstSRV.SampleLevel(SamplerWrap, input.Tex, 0).rgb;
-	float locLum = (0.2126 * tex.r) + (0.7152 * tex.g) + (0.0722 * tex.b);
-	float3 mip = FirstSRV.SampleLevel(SamplerWrap, input.Tex, levels.x).rgb;
-	float avgL = exp(mip.r * log(d + locLum));
+	////follow nuttys code! http://www.nutty.ca/?page_id=352&link=hdr
 
-	float maxL = mip.g;
+	float3 rgb = RGBEToRGB(Texture.Sample(SamplerWrap, input.Tex));
+	float lum = GetLuminance(rgb);
 
-	float lumScale = key / avgL * locLum;
-
-	//follow nuttys code! http://www.nutty.ca/?page_id=352&link=hdr
-	//float max = max(max(tex.r, tex.g), tex.b);
-	//if (max < 0.00001)
-	//	max = 0.0;
-	float finLum = (lumScale * (1 + lumScale / (maxL * maxL))) / (1 + lumScale);
-
-	float3 XYZ = float3x3(0.4124, 0.3576, 0.1805, 0.2126, 0.7152, 0.0722, 0.0193, 0.1192, 0.9505) * float3x1(tex.x, tex.y, tex.z);
-	float3 xyY = float3(XYZ.x / (XYZ.x + XYZ.y + XYZ.z), XYZ.y / (XYZ.x + XYZ.y + XYZ.z), XYZ.y);
-	xyY.z *= finLum;
-
-	XYZ = float3(	xyY.x * (xyY.z / xyY.y), 
-					xyY.z, 
-					(1.0 - xyY.x - xyY.y) * (xyY.z / xyY.y)	);
-
-	float3 RGB = float3x3(	3.2406,		-1.5372,	-0.4986, 
-							-0.9689,	1.8758,		0.0415, 
-							0.0557,		-0.2040,	1.0570)	 * float3x1(XYZ.x, XYZ.y, XYZ.z);
-
-	return float4(RGB.x, RGB.y, RGB.z, 1.0);
-	
-	//return float4(XYZ.x, XYZ.y, XYZ.z, 1.0);
-	//return float4(tex.r, tex.g, tex.b, 1.0);
-	//return float4(avgL, avgL, avgL, 1.0);
-	//return float4(mip.r, mip.g, mip.b, 1.0);
-	
-	
-	//return FirstSRV.SampleLevel(SamplerWrap, input.Tex, levels.x);
+	//return float4(levels.x, levels.y, levels.z, 1.0);
+	////return float4(XYZ.x, XYZ.y, XYZ.z, 1.0);
+	////return float4(tex.r, tex.g, tex.b, 1.0);
+	//return float4(finLum, finLum, finLum, 1.0);
+	////return float4(mip.r, mip.g, mip.b, 1.0);
+	//return float4(rgb, 1.0);
+	return MaxLumSRV.SampleLevel(SamplerWrap, input.Tex, 9);// levels.x);
 
 }
--- Project/Shaders/REINHARD_PixelShader.hlsl
+++ Project/Shaders/REINHARD_PixelShader.hlsl
@@ -1,27 +1,121 @@
 Texture2D FirstSRV;
 sampler SamplerWrap;
 
+cbuffer mipLevel {
+	int4 levels; //x = miplvl, y == 1 = Avg, y == 0 = max
+};
+
 struct VS_OUT {
 	float4 Pos : SV_POSITION;
 	float2 Tex : TEXCOORD;
 };
 
+float ldexp(float x, float exp) {
+	return x * pow(2.0, exp);
+}
+
+float frexp(float x, out float exp) {
+	exp = ceil(log2(x));
+	return(x * exp2(-exp));
+}
+
+float3 RGBEToRGB(float4 rgbe) {
+	if (rgbe.w > 0.0) {
+		rgbe *= 255.0;
+		float val = ldexp(1.0, rgbe.w - (128.0 + 8.0));
+		return rgbe.xyz * val;
+	}
+	return float3(0.0, 0.0, 0.0);
+}
+
+float4 RGBToRGBE(float3 rgb) {
+	float val = max(max(rgb.x, rgb.y), rgb.z);
+	if (val < 0.00001)
+		return float4(0.0, 0.0, 0.0, 0.0);
+	else {
+		float exp;
+		float4 rgbe = float4(0.0, 0.0, 0.0, 0.0);
+		val = frexp(val, exp) * 256.0 / val;
+		rgbe.xyz = rgb.xyz * val;
+		rgbe.w = exp + 128.0;
+		return (rgbe / 255.0);
+	}
+}
+
+float GetLuminance(float3 rgb) {
+	return (0.2126 * rgb.r) + (0.7152 * rgb.g) + (0.0722 * rgb.b);
+}
+
+float3 BilinearFilter(float2 uv, float mipLvlBias) {
+	uint width, heigth;
+	FirstSRV.GetDimensions(width, heigth);
+	uint imagesize = width * heigth;
+	float2 texelSize = float2(1 / width, 1 / heigth);
+	
+	float2 fUv = frac(uv * imagesize);
+	uv = floor(uv * imagesize) / imagesize;
+
+	float3 tl = RGBEToRGB(FirstSRV.SampleLevel(SamplerWrap, uv, mipLvlBias));
+	float3 tr = RGBEToRGB(FirstSRV.SampleLevel(SamplerWrap, uv + float2(texelSize.x, 0.0), mipLvlBias));
+	float3 bl = RGBEToRGB(FirstSRV.SampleLevel(SamplerWrap, uv + float2(0.0, texelSize.y), mipLvlBias));
+	float3 br = RGBEToRGB(FirstSRV.SampleLevel(SamplerWrap, uv + float2(texelSize.x, texelSize.y), mipLvlBias));
+
+	float3 a = lerp(tl, tr, fUv.x);
+	float3 b = lerp(bl, br, fUv.x);
+	return lerp(a, b, fUv.y);
+}
+
+float4 maxFilter(float2 uv, float mipLvlBias) {
+	// try looping through to find brightest
+	uint width, heigth;
+	FirstSRV.GetDimensions(width, heigth);
+	uint imagesize = width * heigth;
+	float2 texelSize = float2(1 / width, 1 / heigth);
+
+	uv = floor(uv * imagesize) / imagesize;
+
+	float4 ptl = FirstSRV.SampleLevel(SamplerWrap, uv, mipLvlBias);
+	float4 ptr = FirstSRV.SampleLevel(SamplerWrap, uv + float2(texelSize.x, 0.0), mipLvlBias);
+	float4 pbl = FirstSRV.SampleLevel(SamplerWrap, uv + float2(0.0, texelSize.y), mipLvlBias);
+	float4 pbr = FirstSRV.SampleLevel(SamplerWrap, uv + float2(texelSize.x, texelSize.y), mipLvlBias);
+
+	float3 tl = RGBEToRGB(ptl);
+	float3 tr = RGBEToRGB(ptr);
+	float3 bl = RGBEToRGB(pbl);
+	float3 br = RGBEToRGB(pbr);
+
+	float ltl = GetLuminance(tl);
+	float ltr = GetLuminance(tr);
+	float lbl = GetLuminance(bl);
+	float lbr = GetLuminance(br);
+
+	float maxLum = max(max(max(ltl, ltr), lbl), lbr);
+	if (ltl == maxLum)
+		return ptl;
+	else if (ltr == maxLum)
+		return ptr;
+	else if (lbl = maxLum)
+		return pbl;
+	else
+		return pbr;
+}
+
 float4 PS_main(VS_OUT input) : SV_TARGET
 {
-	float3 inColor = FirstSRV.Sample(SamplerWrap, input.Tex).rgb;
+	uint width, heigth;
+	FirstSRV.GetDimensions(width, heigth);
+	uint imagesize = width * heigth;
 
-	float avgL = (0.2126 * inColor.r) + (0.7152 * inColor.g) + (0.0722 * inColor.b);
-	float maxL = max(inColor.r, inColor.g);
-	maxL = max(maxL, inColor.b);
-	float minL = min(inColor.r, inColor.g);
-	minL = min(minL, inColor.b);
-	
-	//float red = inColor.r / L;
-	//float green = inColor.g / L;
-	//float blue = inColor.b / L;
-	//L = log(L);
-
-	//store avg in R and max in G
-
-	return float4(avgL, maxL, minL, 1.0f);
+	if (levels.y == 1) {
+		float4 rgb = float4(BilinearFilter(input.Tex, levels.x), 1.0);
+		return RGBToRGBE(rgb);
+	}
+	else {
+		return maxFilter(input.Tex, levels.x);
+		// set a 1x1texture as SRV AND RTV write to it only if the current pixel is brighter than the sample.
+		// Reinhard source code: https://www.cs.utah.edu/~reinhard/cdrom/source.html
+		// https://msdn.microsoft.com/en-us/library/windows/desktop/bb153294(v=vs.85).aspx
+		// http://www.cmap.polytechnique.fr/~peyre/cours/x2005signal/hdr_photographic.pdf
+		// http://www.nutty.ca/?page_id=352&link=hdr
+	}
 }
